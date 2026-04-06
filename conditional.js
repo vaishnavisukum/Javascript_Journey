c5e2ca12e31874598b50b6445243cf00645ee105
vaishnavisukum <vaishnavisukum28@gmail.com>
2026-04-07 00:16:48 +0530

    first lec of javascript

---
+++ conditional.js
@@ -0,0 +1,9 @@
+function if_else(a){
+  var str= "bcdfghjklmnpqrstvwxyz";
+  for(a  str){
+    console.log("is a consonent")
+  }else{
+    console.log("is a vowel")
+  }
+}
+if_else('v')
---
+++ first.js
@@ -0,0 +1,10 @@
+function display(){
+console.log("Jay Ganesh");
+}
+function askname(){
+  console.log("What is your name ? ");
+  var name= prompt("hi ,what is your name");
+  console.log('hello ${name}')
+}
+display();
+askname();
